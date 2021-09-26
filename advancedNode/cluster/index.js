216c678e9c279887aa8f40c9955661940cb54013
udemonia <brandon.l.lambert@icloud.com>
2021-09-26 08:06:42 -0500

    added clustring examples with libuv single thread pool and apache benchmark

--- advancedNode/cluster/index.js
+++ advancedNode/cluster/index.js
@@ -1,29 +1,35 @@
+process.env.UV_THREADPOOL_SIZE = 1
+//? Every Child cluster has a libuv thread pool of 1 instead of 4
 const express = require('express');
 const cluster = require('cluster');
+const crypto = require('crypto');
 
 const app = express();
 
+//? is the file being executed in Master mode? 
 if (cluster.isMaster){
+    //? if yes, execute again in child mode
     cluster.fork()
-
+    cluster.fork()
+    // cluster.fork()
+    // cluster.fork()
+    // cluster.fork()
 } else {
+    //? if not master, handle server functions per usual
     const port = 505;
 
-    const doWork = (duration) => {
-        //! use as much cpu as possible for some duration
-        const start = Date.now()
-        while(Date.now() - start > duration) {
-            
-        }
-    };
-    
-    
     app.get('/', (req, res, next) => {
-        doWork(5000)
+        //! simulating work by calculating a hash
+        crypto.pbkdf2('a', 'b', 10000, 512, 'sha256', () => {
+            console.log('Hash Done!')
+            res.send('Hi Bear 🐻')
+        })
+    });
+    app.get('/fast', (req, res, next) => {
         res.send('Hi Bear 🐻')
     });
-    
     app.listen(port, () => {
         console.log(`Server Up and Listening: `);
     });
 }
+
